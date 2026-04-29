7f972f115a0e4ddf09e014a65af6b304d960c8c9
polring <katack8313@gmail.com>
2026-04-29 19:22:25 +0900

    Feat: add ApiResponse Record

---
+++ backend/promtDeck/src/main/java/org/example/promtdeck/global/common/ApiResponse.java
@@ -0,0 +1,14 @@
+package org.example.promtdeck.global.common;
+
+public record ApiResponse<T>(
+        boolean success,
+        String message,
+        T data
+) {
+    public static <T> ApiResponse<T> sucess(String message,T data) {
+        return new ApiResponse<>(true,message,data);
+    }
+    public static <T> ApiResponse<T> sucess(String message) {
+        return new ApiResponse<>(true, message, null);
+    }
+}
