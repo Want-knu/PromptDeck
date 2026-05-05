ad9eef943fe2e4c94ad0030b1578b150587c76e2
polring <katack8313@gmail.com>
2026-05-05 15:47:02 +0900

    Feat: fix ErrorResponse

--- backend/promtDeck/src/main/java/org/example/promtdeck/global/common/ErrorResponse.java
+++ backend/promtDeck/src/main/java/org/example/promtdeck/global/common/ErrorResponse.java
@@ -1,11 +1,15 @@
 package org.example.promtdeck.global.common;
 
 public record ErrorResponse(
-        boolean sucess,
+        boolean success,
         String code,
         String message
 ) {
-    public static ErrorResponse of(ErrorCode errorCode){
-        return new ErrorResponse(false,errorCode.getCode(),errorCode.getMessage());
+    public static ErrorResponse of(ErrorCode errorCode) {
+        return new ErrorResponse(false, errorCode.getCode(), errorCode.getMessage());
+    }
+
+    public static ErrorResponse of(ErrorCode errorCode, String message) {
+        return new ErrorResponse(false, errorCode.getCode(), message);
     }
 }
