b13331196c14e4187076fcf0ba8947f8628458b1
polring <katack8313@gmail.com>
2026-05-23 21:17:39 +0900

    feat: add provider execution presets

---
+++ backend/promtDeck/src/main/java/org/example/promtdeck/domain/provider/repository/ProviderExecutionPresetRepository.java
@@ -0,0 +1,7 @@
+package org.example.promtdeck.domain.provider.repository;
+
+import org.example.promtdeck.domain.provider.entity.ProviderExecutionPreset;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProviderExecutionPresetRepository extends JpaRepository<ProviderExecutionPreset, Long> {
+}
